57668cd88c9d205f9e2e721b4b57339e6d319d3e
Tom Scott <tomscott292@gmail.com>
2019-01-23 14:50:50 +0000

    Moves constants to config.ini

--- rpi-coap.py
+++ rpi-coap.py
@@ -10,17 +10,14 @@ import Adafruit_DHT
 from json import dumps
 from socket import gethostbyname, gaierror
 from time import sleep
-from socket import gethostbyname, gaierror
+import configparser
+from shutil import copy
+import os
 
 from coapthon.client.helperclient import HelperClient
 from coapthon.utils import parse_uri
 
 _SENSOR = Adafruit_DHT.AM2302
-_GPIO_PIN = 4
-_HOST = "demo.thingsboard.io"
-_DEVICE_AUTH_TOKEN = "9tQn151djuEdRX0ucu0k"
-_PORT = 5683 # Default CoAP port
-_SLEEP_INTERVAL = 5
 
 class SensorData(object):
     """
@@ -53,7 +50,7 @@ class SensorData(object):
         temperature_string = 'Temp={0:.1f}*C'.format(self.temperature)
         return humidity_string + ' ' + temperature_string
 
-def get_sensor_data():
+def get_sensor_data(gpio_pin):
     """
     Returns a SensorData object containing the latest data 
     from the DHT22 sensor.
@@ -64,28 +61,28 @@ def get_sensor_data():
     # Try to grab a sensor reading.  Use the read_retry method 
     # which will retry up to 15 times to get a sensor reading 
     # (waiting 2 seconds between each retry).
-    humidity, temperature = Adafruit_DHT.read_retry(_SENSOR, _GPIO_PIN)
+    humidity, temperature = Adafruit_DHT.read_retry(_SENSOR, gpio_pin)
 
     return SensorData(humidity, temperature)
 
-def get_coap_client():
+def get_coap_client(host, port):
     """
     Procedure for creating the CoAP client.
     Attempts to get IP address for host from the host name.
     Returns CoAP HelperClient
     """
-    host = _HOST
+    _host = host
 
     # Try to get ip address
     try:
-        tmp = gethostbyname(host)
-        host = tmp
+        tmp = gethostbyname(_host)
+        _host = tmp
     except gaierror:
         # use domain if cannot get ip
         pass
 
     # create CoAP client
-    return HelperClient(server=(host, _PORT))
+    return HelperClient(server=(_host, port))
 
 def send_data(sensor_data, client, path):
     """
@@ -100,13 +97,26 @@ def send_data(sensor_data, client, path):
     # Print response from cloud to console
     print(response.pretty_print())
 
-def main():
+def main(config):
     """ Main entry point of the app """
 
     # Create path that the data will be sent to
-    path = "api/v1/" + _DEVICE_AUTH_TOKEN + "/telemetry"
+    auth_token = config.get('custom', 'device_auth_token')
+
+    # Raise an error if the auth token is not defined in config file
+    if not auth_token:
+        raise ValueError('Device auth token not present, please update config.ini')
+
+    path = "api/v1/" + auth_token + "/telemetry"
+
     # Create a CoAP client
-    client = get_coap_client()
+    client = get_coap_client(
+        config.get('custom', 'host'),
+        config.getint('custom', 'port'))
+
+    # Make sure sleep interval is at least one second
+    sleep_interval = config.getint('custom', 'sleep_interval')
+    sleep_interval = sleep_interval if sleep_interval >= 1 else 1 
 
     try:
         while True:
@@ -114,14 +124,14 @@ def main():
             # Note that sometimes you won't get a reading and
             # the results will be null (because Linux can't
             # guarantee the timing of calls to read the sensor).
-            sensor_data = get_sensor_data()
+            sensor_data = get_sensor_data(config.getint('custom', 'gpio_pin'))
 
             if sensor_data.has_data():
                 # Format the data to JSON to be sent
                 send_data(sensor_data, client, path)
             
             # Wait specified time and repeat
-            sleep(_SLEEP_INTERVAL)
+            sleep(sleep_interval)
 
     # Allow Ctrl + C to stop the script
     except KeyboardInterrupt:
@@ -129,6 +139,18 @@ def main():
         client.stop()
         exit(0)
 
+def get_config():
+    if not os.path.exists('config.ini'):
+        if os.path.exists('config.ini.example'):
+            copy('config.ini.example', 'config.ini')
+        else:
+            raise FileNotFoundError('No config.ini or config.ini.example found.')
+        
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config
+
 if __name__ == "__main__":
     """ This is executed when run from the command line """
-    main()
+    config = get_config()
+    main(config)
