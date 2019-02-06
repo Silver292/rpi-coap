e69cc738f6cb8f1b92c9201a7924521957753fe3
Tom Scott <tomscott292@gmail.com>
2019-02-06 16:44:31 +0000

    Moves SensorData Class to it's own file.

--- rpicoap/rpicoap.py
+++ rpicoap/rpicoap.py
@@ -21,6 +21,7 @@ import sys
 from coapthon.client.helperclient import HelperClient
 import logging
 import csv
+from rpicoap.sensordata import SensorData
 
 # Set default logging level for imported module
 logging.getLogger('coapthon').setLevel(logging.WARNING)
@@ -40,37 +41,6 @@ except ImportError:
         AM2302 = 1)
 
 
-
-class SensorData(object):
-    """
-    Data object containing humidity and temperature data.
-    """
-
-    def __init__(self, humidity, temperature):
-        """
-        Set humidity and temperature
-        """
-        self.humidity = humidity
-        self.temperature = temperature
-
-    def has_data(self):
-        """
-        Returns True if humidity and temperature are not None.
-        """
-        return self.humidity is not None and \
-               self.temperature is not None
-
-    def as_json(self):
-        """
-        Returns the humidity and temperature data as a JSON string.
-        """
-        return json.dumps({'humidity': self.humidity, \
-                'temperature': self.temperature})
-
-    def __str__(self):
-        humidity_string = 'Humidity={0:.1f}%'.format(self.humidity)
-        temperature_string = 'Temp={0:.1f}*C'.format(self.temperature)
-        return humidity_string + ' ' + temperature_string
 
 def get_sensor_data(sensor, gpio_pin):
     """
---
+++ rpicoap/sensordata.py
@@ -0,0 +1,32 @@
+import json
+
+class SensorData(object):
+    """
+    Data object containing humidity and temperature data.
+    """
+
+    def __init__(self, humidity, temperature):
+        """
+        Set humidity and temperature
+        """
+        self.humidity = humidity
+        self.temperature = temperature
+
+    def has_data(self):
+        """
+        Returns True if humidity and temperature are not None.
+        """
+        return self.humidity is not None and \
+               self.temperature is not None
+
+    def as_json(self):
+        """
+        Returns the humidity and temperature data as a JSON string.
+        """
+        return json.dumps({'humidity': self.humidity, \
+                'temperature': self.temperature})
+
+    def __str__(self):
+        humidity_string = 'Humidity={0:.1f}%'.format(self.humidity)
+        temperature_string = 'Temp={0:.1f}*C'.format(self.temperature)
+        return humidity_string + ' ' + temperature_string
